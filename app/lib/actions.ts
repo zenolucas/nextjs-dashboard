6c8e1838d8372ddcfa241f70f1342df4819b3f18
Zen Lucas <2200501@slu.edu.ph>
2025-06-18 15:22:14 +0800

    continued Chapter 12, but I'm getting an error: An error occurred in the Server Components render but no message was provided

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,13 @@
 'use server'; // mark all exported functions within the files as Server Actions.
 
 
-export async function createInvoice(formData: FormData) {}
+// extracting the data from formData
+export async function createInvoice(formData: FormData) {
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+
+    console.log(rawFormData)
+}
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -9,6 +9,7 @@ import {
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
 
+// creating a form to capture the user's input
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
     <form action={createInvoice}>
